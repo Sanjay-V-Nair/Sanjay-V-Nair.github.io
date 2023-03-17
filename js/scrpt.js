ecc7e496971bbcf770350175b9200fc85f3fe482
Sanjay-V-Nair <sanjay.v2021@vitbhopal.ac.in>
2023-03-17 15:26:16 +0530

    scripts added inside html itself and social media links updated

--- js/scrpt.js
+++ js/scrpt.js
@@ -1,18 +1,18 @@
 
-// STRING TYPER
-var typed = new Typed('#typer',{
-    strings: ['Frontend Develpoer', 'Designer', 'AR/VR Entusiast'],
-    typespeed:100,
-    backspeed:100,
-    backspeed:1000,
-    loop:true
-});
+// // STRING TYPER
+// var typed = new Typed('#typer',{
+//     strings: ['Frontend Develpoer', 'Designer', 'AR/VR Entusiast'],
+//     typespeed:100,
+//     backspeed:100,
+//     backspeed:1000,
+//     loop:true
+// });
 
-//Scroll reveal
-ScrollReveal({ 
-    reset: true,
-    distance:'80px',
-    duration: 2000,
-    delay: 200
-});
-ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+// //Scroll reveal
+// ScrollReveal({ 
+//     reset: true,
+//     distance:'80px',
+//     duration: 2000,
+//     delay: 200
+// });
+// ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
